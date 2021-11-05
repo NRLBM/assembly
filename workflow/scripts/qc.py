c7794b279977b4f8f6a1097a86c94bed0c5af954
boasvdp <boasvdp@gmail.com>
2021-11-05 15:59:03 +0100

    Add typing

---
+++ workflow/scripts/qc.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+
+import argparse
+
+parser = argparse.ArgumentParser(description='Summarise assembled isolates')
+
+parser.add_argument("--species", required=True, help="Species to select samples for", type=str)
+parser.add_argument('--timestamp', required=True, help='Timestamp to parse results for', type=str)
+parser.add_argument("--thresholds", dest='thresholds', help='Quality control thresholds for sample selection', type=str, default='workflow/config/qc_thresholds.json')
+parser.add_argument("--exclude", dest='exclude', help='Specific samples to exclude regardless of QC', nargs='+', default = [])
+parser.add_argument('--output', required=True, dest='output', help='Destination of output', type=str)
+
+args = parser.parse_args()
+
+import pandas as pd
+import numpy as np
+import json
+
+def select_all_samples_for_species(timestamp, species):
+  summary_path = '/'.join(['backup', timestamp, 'summary.csv'])
+  df = pd.read_csv(summary_path)
+  df_species = df.query('`Top species` == @species')
+  return df_species
+
+def read_qc_filter_thresholds(species, thresholds_file):
+  with open(thresholds_file) as file:
+    all_qc_thresholds = json.load(file)
+  qc_thresholds_species = all_qc_thresholds[species]
+  return qc_thresholds_species
+
+def qc_filter_species(df_species, thresholds_dict):
+  min_N50, min_genome_size, max_genome_size, min_sequencing_depth, max_number_contigs = thresholds_dict['min_N50'], thresholds_dict['min_genome_size'], thresholds_dict['max_genome_size'], thresholds_dict['min_sequencing_depth'], thresholds_dict['max_number_contigs']
+  df_out = pd.DataFrame()
+  df_out['Sample'] = df_species['Sample']
+  df_out['N50'] = np.select([df_species['N50'] >= min_N50], ['PASS'], default='FAIL')
+  df_out['Total assembly size'] = np.select([(df_species['Total assembly size'] >= min_genome_size) & (df_species['Total assembly size'] <= max_genome_size)], ['PASS'], default='FAIL')
+  df_out['Sequencing depth'] = np.select([df_species['Sequencing depth'] >= min_sequencing_depth], ['PASS'], default='FAIL')
+  df_out['Number of contigs'] = np.select([df_species['Number of contigs'] <= max_number_contigs], ['PASS'], default='FAIL')
+  df_out['Decision'] = np.select([(df_out.iloc[:,1:] == 'PASS').all(axis=1)], ['PASS'], default='FAIL')
+  return df_out
+
+def remove_specific_samples(df_qc_filtered, exclusion_list):
+  if len(exclusion_list) == 0:
+    return df_qc_filtered
+  else:
+    df_qc_filtered.loc[df_qc_filtered['Sample'].isin(exclusion_list), 'Decision'] = 'MANUALLY_EXCLUDED'
+    return df_qc_filtered
+
+def save_output(df_out, output):
+  df_out.to_csv(output, sep = '\t', index=False)
+
+def main(args):
+  df_species = select_all_samples_for_species(args.timestamp, args.species)
+  thresholds_dict = read_qc_filter_thresholds(args.species, args.thresholds)
+  df_qc_filtered = qc_filter_species(df_species, thresholds_dict)
+  df_qc_filtered_excluded = remove_specific_samples(df_qc_filtered, args.exclude)
+  save_output(df_qc_filtered_excluded, args.output)
+
+if __name__ == '__main__':
+  main(args)
---
+++ workflow/scripts/typing_summary.py
@@ -0,0 +1,163 @@
+#!/usr/bin/env python3
+
+import argparse
+import pandas as pd
+
+parser = argparse.ArgumentParser(description='Summarise typed isolates')
+
+parser.add_argument('samples', nargs='+', help="List of sample names to summarise", type=str)
+parser.add_argument('--species', dest='species', help="Species to summarise", choices=['Escherichia coli', 'Neisseria meningitidis', 'Ecoli', 'Nmen'], type=str, required=True)
+parser.add_argument('--mlst', dest="mlst", help="mlst output directory", type=str)
+parser.add_argument('--gmats', dest="gmats", help="gMATS output directory", type=str, default="gMATS_Nmen")
+parser.add_argument('--amrfinder', dest="amrfinder", help="AMRfinder output directory", type=str)
+parser.add_argument('--ectyper', dest="ectyper", help="ECtyper output directory", type=str, default="ectyper_Ecoli")
+parser.add_argument('--fimtyper', dest="fimtyper", help="ABRicate Fimtyper output directory", type=str, default="ABRicate_fimH_Ecoli")
+parser.add_argument('--vfdb', dest="vfdb", help="ABRicate VFDB output directory", type=str)
+parser.add_argument('--timestamp', dest="timestamp", help="Timestamp of analysis", type=str, required=True)
+parser.add_argument('--output', dest='output', help="Output file", type=str, required=True)
+
+args = parser.parse_args()
+
+if (args.species == "Escherichia coli") or (args.species == "Ecoli"):
+  args.species == "Ecoli"
+  assert args.mlst is not None
+  assert args.amrfinder is not None
+  assert args.ectyper is not None
+  assert args.fimtyper is not None
+  assert args.vfdb is not None
+elif (args.species == "Neisseria meningitidis") or (args.species == "Nmen"):
+  args.species == "Nmen"
+  assert args.mlst is not None
+  assert args.amrfinder is not None
+  assert args.vfdb is not None
+  assert args.gmats is not None
+
+# Store samples as isolate_list
+isolate_list = list(args.samples)
+
+# Make output prefix based on timestamp
+output_prefix = 'tmp_data/' + str(args.timestamp) + '/'
+
+def customreadlines(file):
+  '''
+  Read all lines of a file and close connection.
+
+  Parameters
+  ----------
+  file : str
+    File name to open
+
+  Returns
+  -------
+  tmp.readlines() : list
+    List containing all lines
+
+  '''
+  tmp = open(file)
+  return tmp.readlines()
+  tmp.close()
+
+def parse_amrfinder(gene, lines, WT):
+  tmp_list = []
+  for line in lines:
+    if gene in line.split('\t')[5]:
+      tmp_list.append(line.split('\t')[5])
+  if len(tmp_list) == 0:
+    tmp_list.append(WT)
+  output_string = '|'.join(tmp_list)
+  return output_string
+
+def parse_vfdb(gene, lines):
+  tmp_list = []
+  for line in lines:
+    if gene in line.split('\t')[5]:
+      tmp_list.append(line.split('\t')[5])
+  if len(tmp_list) == 0:
+    output_string = 'None'
+  else:
+    tmp_list.sort()
+    output_string = gene
+  for i in tmp_list:
+    output_string = output_string + str(i[-1:])
+  return output_string
+
+def process_ecoli(args):
+  full_df = pd.DataFrame()
+
+  tuple_resistance_genes = ('blaCTX-M', 'blaOXA', 'blaCMY', 'gyrA_S83', 'gyrA_D87', 'parC_S80')
+  tuple_VFDB_genes = ('asl', 'cnf', 'fde', 'ibe', 'kps', 'neu', 'sfa', 'hly', 'iro', 'iut', 'iuc', 'pap', 'ybt')
+
+  for isolate in isolate_list:
+    # Get ST
+    filepath = output_prefix + args.mlst + '/' + isolate + ".tsv"
+    isolate_df = pd.read_csv(filepath, sep='\t')
+
+    # Get AMR genes and put lines in a list to reuse
+    lines = customreadlines(output_prefix + args.amrfinder + '/' + isolate + ".tsv")
+
+    for resistance_gene in tuple_resistance_genes:
+      isolate_df[resistance_gene] = parse_amrfinder(resistance_gene, lines, '-')
+
+    ############## ECTYPER ################
+    filepath = output_prefix + args.ectyper + '/' + isolate + "/output.tsv"
+    ectyper_df = pd.read_csv(filepath, sep='\t')
+    ectyper_df = ectyper_df.drop(['Name', 'Species', 'QC', 'AlleleKeys', 'GeneContigNames', 'GeneRanges', 'GeneLengths'], axis=1)
+
+    isolate_df = pd.concat([isolate_df, ectyper_df], axis=1)
+
+    # ABRicate fimH typing
+    lines = customreadlines(output_prefix + args.fimtyper + '/' + isolate + '.tsv')
+    isolate_df['fimH'] = parse_amrfinder('fimH', lines, 'None')
+
+    # ABRicate VFDB typing
+    lines = customreadlines(output_prefix + args.vfdb + "/" + isolate + ".tsv")
+
+    for VFDB_gene in tuple_VFDB_genes:
+      isolate_df[VFDB_gene] = parse_vfdb(VFDB_gene, lines)
+
+    full_df = full_df.append(isolate_df)
+
+  return full_df
+
+def process_nmen(args):
+  full_df = pd.DataFrame()
+
+  tuple_resistance_genes = ('penA_A311', 'penA_I312', 'penA_V316', 'penA_D346D', 'penA_T483', 'penA_A501', 'penA_F504', 'penA_A510', 'penA_N512', 'penA_A516', 'penA_G542', 'penA_G545', 'penA_P551', 'folP_R228', 'mtrR_A39', 'mtrR_G45', 'gyrA_S91', 'gyrA_D95', 'gyrB_D429', 'gyrB_K450', 'parC_D86', 'parC_S87', 'parC_S88', 'parC_E91', 'parE_G410', 'ponA_L421', 'porB1b_G120', 'porB1b_A121', 'rplD_G70', 'rplV_K83KKGPSL', 'rplV_K90KARA', 'rpoB_G158', 'rpoB_P157', 'rpoB_R201', 'rpoD_DDDA92de', 'rpoD_E98', 'rpsE_K28', 'rpsE_T24', 'rpsE_V27de', 'rpsJ_V57', '16S_C1191', '23S_A2045', '23S_A2057', '23S_A2145', '23S_C2597', 'macAB_G-48', 'mtrC_C-120', 'mtrR_A-53', 'mtrR_A-56', 'mtrR_G-131', 'norM_A-7', 'norM_C-104')
+  tuple_VFDB_genes = ('ctr', 'far', 'fbp', 'fHbp', 'hmb', 'hpu', 'iga', 'kat', 'lbp', 'lip', 'mnt', 'mtr', 'opc', 'pil', 'por', 'sia')
+
+  for isolate in isolate_list:
+    # Get ST
+    filepath = output_prefix + args.mlst + '/' + isolate + ".tsv"
+    isolate_df = pd.read_csv(filepath, sep='\t')
+
+    ### GMATS
+    filepath = output_prefix + args.gmats + '/' + isolate + ".tsv"
+    gmats_df = pd.read_csv(filepath, sep='\t')
+    gmats_df = gmats_df.drop(['Isolate'], axis=1)
+
+    isolate_df = pd.concat([isolate_df, gmats_df], axis=1)
+
+    # Get AMR genes and put lines in a list to reuse
+    lines = customreadlines(output_prefix + args.amrfinder + '/' + isolate + ".tsv")
+
+    for resistance_gene in tuple_resistance_genes:
+      isolate_df[resistance_gene] = parse_amrfinder(resistance_gene, lines, '-')
+
+    # ABRicate VFDB typing
+    lines = customreadlines(output_prefix + args.vfdb + "/" + isolate + ".tsv")
+
+    for VFDB_gene in tuple_VFDB_genes:
+      isolate_df[VFDB_gene] = parse_vfdb(VFDB_gene, lines)
+
+    full_df = full_df.append(isolate_df)
+
+  return full_df
+
+
+if args.species == "Ecoli":
+  full_df = process_ecoli(args)
+  full_df.to_csv(args.output, sep = '\t', index=False)
+
+if args.species == "Nmen":
+  full_df = process_nmen(args)
+  full_df.to_csv(args.output, sep = '\t', index=False)
